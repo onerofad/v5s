eba159fac6c792e6f6b4e54b5919f53333b24d9f
frank <frank@gmail.com>
2026-02-11 00:39:01 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { createMedia } from "@artsy/fresnel";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
-function App() {
+const {Media, MediaContextProvider} = createMedia({
+  breakpoints: {
+    mobile: 0,
+    tablet: 768,
+    desktop: 1024
+
+  }
+})
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MediaContextProvider>
+      <Media greaterThan="mobile">
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Media>
+      <Media at="mobile">
+        <BrowserRouter>
+          <Routes>
+            <Route  index element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+
+      </Media>
+    </MediaContextProvider>
+    
+  )
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import { Grid, GridColumn, Header } from "semantic-ui-react";
+
+const Home = () => (
+    <Grid>
+        <GridColumn>
+            <Header>Website is coming</Header>
+        </GridColumn>
+    </Grid>
+)
+export default Home
