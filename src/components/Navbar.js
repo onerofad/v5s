6e279f3bc2821e551f0e9b3bb25e6db8e33c2a65
frank <frank@gmail.com>
2026-02-11 00:51:35 -0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Menu, MenuItem } from "semantic-ui-react";
+
+const Navbar = () => (
+    <Menu
+        secondary
+        size="large"
+        fixed="top"
+    >
+        <MenuItem>
+        </MenuItem>
+    </Menu>
+)
